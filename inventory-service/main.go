48761b4ec3275b80d9c2a2e2c158ebb35ed71adb
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-10-08 18:27:55 -0300

    added get method for products

--- inventory-service/main.go
+++ inventory-service/main.go
@@ -1,21 +1,74 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-type fooHandler struct {
-	Message string
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"ski"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []Product
+
+func init() {
+	productsJSON := `[
+		{
+			"productId": 1,
+			"manufacturer": "Johns-Jenkins",
+			"sku": "p5z343vds",
+			"upc": "13123234324",
+			"pricePerUnit": "497.45",
+			"quantityOnHand": 9703,
+			"productName": "sticky note"
+			},
+		{
+			"productId": 2,
+			"manufacturer": "Hessel",
+			"sku": "p5z343ass",
+			"upc": "13122223324",
+			"pricePerUnit": "197.45",
+			"quantityOnHand": 703,
+			"productName": "leg warmeres"
+		},
+		{
+			"productId": 3,
+			"manufacturer": "Swaniawski",
+			"sku": "p5z3ff34s",
+			"upc": "1312221239",
+			"pricePerUnit": "457.45",
+			"quantityOnHand": 403,
+			"productName": "lamp shade"
+		}	
+	]`
+	err := json.Unmarshal([]byte(productsJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("bar called"))
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJSON, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJSON)
+
+	}
 }
 
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo called"})
-	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/products", productsHandler)
 	http.ListenAndServe(":5000", nil)
 }
