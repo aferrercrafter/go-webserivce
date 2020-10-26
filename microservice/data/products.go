ce29ae9421bf48be0187cd5278ca4fd72ce03e71
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-10-25 21:54:33 -0300

    added product model and dummy data

---
+++ microservice/data/products.go
@@ -0,0 +1,36 @@
+package data
+
+import "time"
+
+// Product defines the structure for an API product
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+var productsLists = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Esspresso",
+		Description: "Short and strong codee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
