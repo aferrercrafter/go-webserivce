0c7a243d842746bd0be99efdfeaeccbb1040c283
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-10-25 21:42:21 -0300

    added gracefully shotdown

--- microservice/main.go
+++ microservice/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/aferrercrafter/microservice-go/handlers"
@@ -26,5 +28,22 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
 	s.ListenAndServe()
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
