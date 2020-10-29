6f1dcd4dbe183a0c6c0620cda87383b1c0e11d2f
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-10-29 20:21:52 -0300

    added validation for price and sku

--- microservice/data/products.go
+++ microservice/data/products.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -14,8 +15,8 @@ type Product struct {
 	ID          int     `json:"id"`
 	Name        string  `json:"name" validate:"required"`
 	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	SKU         string  `json:"sku"`
+	Price       float32 `json:"price" validate:"gt=0"`
+	SKU         string  `json:"sku" validate:"required,sku"`
 	CreatedOn   string  `json:"_"`
 	UpdatedOn   string  `json:"_"`
 	DeletedOn   string  `json:"_"`
@@ -32,7 +33,19 @@ func (p *Products) ToJSON(w io.Writer) error {
 
 func (p *Product) Validate() error {
 	validate := validator.New()
+	validate.RegisterValidation("sku", validateSKU)
 	return validate.Struct(p)
+}
+
+func validateSKU(fl validator.FieldLevel) bool {
+	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
+	matches := re.FindAllString(fl.Field().String(), -1)
+
+	if len(matches) != 1 {
+		return false
+	}
+
+	return true
 }
 
 // FromJSON decode and write the products
